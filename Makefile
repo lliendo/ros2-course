96bf50dd205e37a302ac7fbc8d10edf21fa13cc7
Lucas Liendo <lucas.liendo@eliflab.com>
2022-07-04 03:28:16 +0200

    Add `gazebo_launch.py` and update robot model [2.2]

--- Makefile
+++ Makefile
@@ -1,14 +1,22 @@
-.PHONY: build start-rviz2 clean
+.PHONY: build start-rviz2 start-simulation vel-test clean
 
 SHELL := /usr/bin/bash
 
 build:
 	rosdep install -i --from-path src --rosdistro galactic -y
 	colcon build --symlink-install --packages-select \
-	    dribot_description
+	    dribot_description \
+	    dribot_simulation
 
 start-rviz:
 	source install/setup.bash && ros2 launch dribot_description rviz_launch.py
+
+start-simulation:
+	source install/setup.bash && ros2 launch dribot_simulation gazebo_launch.py
+
+# Run a velocity test command.
+vel-test:
+	./scripts/vel-test.sh
 
 clean:
 	rm -rf build log install
---
+++ src/dribot_simulation/launch/gazebo_launch.py
@@ -0,0 +1,51 @@
+from os.path import join
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.substitutions import FindPackageShare
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+def get_gazebo_launcher():
+    return IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            join(get_package_share_directory('gazebo_ros'), 'launch'),
+            '/gazebo.launch.py'
+        ]),
+    )
+
+def get_spawn_entity():
+    return Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-entity', 'dribot', '-topic', '/robot_description'],
+        output='screen',
+    )
+
+def get_robot_state_node():
+    pkg_share = FindPackageShare(package='dribot_description').find('dribot_description')
+
+    return IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            join(pkg_share, 'launch/load_description_launch.py'),
+        ])
+    )
+
+def generate_launch_description():
+    # Uncomment for manually controlling the robot.
+    #teleop_keyboard = Node(
+    #    package='teleop_twist_keyboard',
+    #    executable='teleop_twist_keyboard',
+    #    output='screen',
+    #    prefix='xterm -e',
+    #)
+
+    return LaunchDescription([
+        get_gazebo_launcher(),
+        get_spawn_entity(),
+        get_robot_state_node(),
+        #teleop_keyboard,
+    ])
